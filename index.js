9993112295011c3b389f92383c392187b77a60e3
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-06 00:49:17 -0400

    expose static dir

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ fs.readdirSync("./endpoints/").forEach(function (file) {
 });
 
 app.use(express.static('./client/dist', { extensions: ["html"] }));
+app.use('/static', express.static('./client/static'));
 
 app.use('/', function (req, res) {
     res.sendFile(path.join(__dirname + `/client/dist/index.html`));
