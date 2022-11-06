32c888586e16fb0b52ed2bc8d1c96ccda5099a10
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-06 01:18:25 -0500

    make home btns work

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -1,21 +1,32 @@
 import * as React from "react";
 
 import { SessionContext } from "../../util/session";
+import { useNavigate } from "react-router-dom";
 
 import "./index.scss";
 
 export const Home = () => {
     const session = React.useContext(SessionContext);
+    const nav = useNavigate();
 
     return (
         <div className={"home"}>
             <div id={"home-content"}>
                 <h2>JEFF COIN</h2>
                 <p><br></br>{session.user.loggedin ? <>
-                    <button type="button" className="btn btn-primary">Go to Wallet</button>
+                    <button type="button" className="btn btn-primary" onClick={(ev) => {
+                        ev.preventDefault();
+                        nav("/wallet");
+                    }}>Go to Wallet</button>
                 </> : <>
-                    <button type="button" className="btn btn-primary">Register</button>
-                    <button type="button" className="btn btn-primary">Log in</button>
+                    <button type="button" className="btn btn-primary" onClick={(ev) => {
+                        ev.preventDefault();
+                        nav("/register");
+                    }}>Register</button>
+                    <button type="button" className="btn btn-primary" onClick={(ev) => {
+                        ev.preventDefault();
+                        nav("/login");
+                    }}>Log in</button>
                 </>}</p>
                 <p>I can't make these buttons go down... just like how I don't know how to make &#91;redacted&#93; go down after seeing this background</p>
             </div>
