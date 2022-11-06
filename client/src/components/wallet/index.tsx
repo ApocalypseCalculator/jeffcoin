45dfaf9ccb529fc6c7f91cd0d870f2e73d4b5f90
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-06 16:40:45 -0500

    add wallet page

--- client/src/components/wallet/index.tsx
+++ client/src/components/wallet/index.tsx
@@ -4,6 +4,7 @@ import * as axios from "axios";
 import { SessionContext } from "../../util/session";
 
 import "./index.scss";
+import Link from "../../util/link";
 
 export const Wallet = () => {
     const session = React.useContext(SessionContext);
@@ -36,12 +37,89 @@ export const Wallet = () => {
                 {
                     userinfo.wallet == -1 ? <>
                         <h3>Loading...</h3>
-                    </> : <h3>Your balance is {userinfo.wallet} Jeff coins!</h3>
+                    </> : <>
+                        <h3>Your balance is {userinfo.wallet} Jeff coins!</h3>
+                        <h4>Your wallet ID is <b>{session.user.userid}</b></h4>
+                    </>
                 }
             </div>
             <div className={"container"}>
-                <p>Table of your recent transactions</p>
+                <p>Your recent sent transactions</p>
+                <div className={"container"}>
+                    <table id={"myTable"}>
+                        <tr className={"header"}>
+                            <th>ID</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Amount</th>
+                            <th>Status</th>
+                            <th>Time</th>
+                            <th>Block ID</th>
+                        </tr>
+                        {
+                            userinfo.wallet == -1 ?
+                                <tr id={"status"}>
+                                    <td>Loading...</td>
+                                </tr>
+                                :
+                                <GenerateTable transactions={userinfo.transactionsFrom} />
+                        }
+                    </table>
+                </div>
+            </div>
+            <div className={"container"}>
+                <p>Your recent received transactions</p>
+                <div className={"container"}>
+                    <table id={"myTable"}>
+                        <tr className={"header"}>
+                            <th>ID</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Amount</th>
+                            <th>Status</th>
+                            <th>Time</th>
+                            <th>Block ID</th>
+                        </tr>
+                        {
+                            userinfo.wallet == -1 ?
+                                <tr id={"status"}>
+                                    <td>Loading...</td>
+                                </tr>
+                                :
+                                <GenerateTable transactions={userinfo.transactionsTo} />
+                        }
+                    </table>
+                </div>
             </div>
         </div>
     )
+}
+
+function GenerateTable(props: any) {
+    let table = props.transactions.map((trans: any) => {
+        return (<>
+            <tr>
+                <td className="breakname">{trans.id}</td>
+                <td className="breakname">{trans.fromid}</td>
+                <td className="breakname">{trans.toid}</td>
+                <td className="breakname">{trans.amount}</td>
+                <td className="breakname">{
+                    ["Success", "Mining in progress", "Queued", "Failed"][trans.status]
+                }</td>
+                <td>{new Date(trans.createtime).toLocaleString()}</td>
+                <td>
+                    {
+                        trans.status > 1 ? "-" : <Link class="breakname" href={`/block?id=${trans.blockid}`} text={trans.blockid} />
+                    }
+                </td>
+            </tr>
+        </>);
+    });
+    table = table.filter((t: any) => t);
+    if (table.length == 0) {
+        return (<tr><td>No transactions to display</td></tr>);
+    }
+    else {
+        return (<>{table}</>);
+    }
 }
