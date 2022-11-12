8093dc491a0a1f647cf249af8f19abc20003adb4
Patrick Lin <apocalypsecalculator@gmail.com>
2022-11-12 15:13:10 -0500

    add block list endpoint

---
+++ endpoints/apiblocklist.js
@@ -0,0 +1,34 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const config = require('../config');
+const jwt = require('jsonwebtoken');
+
+module.exports.name = "/api/block/list";
+module.exports.method = "GET";
+module.exports.verify = function (req, res) {
+    return true;
+}
+
+module.exports.execute = function (req, res) {
+    let user = null;
+    try {
+        user = jwt.verify(req.headers.authorization, config.secrets.jwt);
+    }
+    catch { }
+    if (!user) {
+        res.status(401).json({ status: 401, error: 'Unauthorized' });
+    }
+    else {
+        prisma.block.findMany({
+            skip: req.query.offset ?? 0,
+            take: 10,
+            orderBy: {
+                minetime: 'desc'
+            }
+        }).then(result => {
+            res.json(result);
+        }).catch(() => {
+            res.status(500).json({ status: 500, error: 'Internal server error' });
+        })
+    }
+}
