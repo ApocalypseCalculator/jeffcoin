660a537388a356d583fe522ba510658c150e43b2
Patrick Lin <apocalypsecalculator@gmail.com>
2022-12-06 21:47:17 -0500

    quotes file

---
+++ quotes.js
@@ -0,0 +1,13 @@
+//These are quotes that appear on the home page. Toggling "on" will disable them
+module.exports = {
+    on: true,
+    quotes: [
+        "Hey @everyone! Have a paw-ssion for feet?",
+        "I love Chick-fil-A",
+        "Wait guys... How do I do dot product and cross product again???",
+        "You are a darker shade",
+        "low d high minus high d low, square the bottom and here you go",
+        "the derivative of e^x is xe^(x-1)",
+        "centimeter?? is that like, 10 meters?"
+    ]
+}
